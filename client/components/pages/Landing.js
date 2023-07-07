2f95e503ca17db1bfbf7069fb4d9d1198493f2bf
anyaguuu <ag156@rice.edu>
2023-07-07 09:33:05 -0500

    components, pages, util folders

---
+++ client/components/pages/Landing.js
@@ -0,0 +1,17 @@
+import owl from './owl.png'
+import './Landing.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={owl} className="App-logo" alt="logo" style={{ width: '10vw', height: 'auto'}}/>
+        <p>
+          Welcome to Owlmart!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
