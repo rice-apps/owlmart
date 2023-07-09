ca29222116f8fd0f3feaf027e7e46c7c06907c5a
Daniel Cufino <dc58@rice.edu>
2023-07-09 11:06:09 -0500

    Fix Silly Imports

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { ApolloClient } from '@apollo/client';
-import { InMemoryCache } from '@apollo/client';
-import { createHttpLink } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
+import {
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+  ApolloClient,
+} from '@apollo/client';
+
 import App from './components/pages/Landing';
 
 const httpLink = createHttpLink({
