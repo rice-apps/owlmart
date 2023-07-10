8451ec56781cf5cf88f4f3cdb9d17ceb98bc288f
Aaron Duong <duongaaron03@gmail.com>
2023-07-09 21:30:37 -0500

    Added nav bar

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,13 +1,32 @@
-import owl from './owl.png'
+import owl from './owl.png';
 import './Landing.css';
+import { Home } from '@material-ui/icons';
 
 function App() {
   return (
     <div className="App">
+
+      <nav>
+        <ul>
+          <li>
+            <a href="Home.js">Home</a>
+          </li>
+
+          <li>
+            <a href="Favorites.js">Favorites</a>
+          </li>
+
+          <li>
+            <a href="Cart.js">Cart</a>
+          </li>
+        </ul>
+        
+      </nav>
+
       <header className="App-header">
         <img src={owl} className="App-logo" alt="logo" style={{ width: '10vw', height: 'auto'}}/>
         <p>
-          Welcome to Owlmart!!
+          Hello
         </p>
       </header>
     </div>
