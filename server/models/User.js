8a7911c1672ce15c4ce47f5ca1c79d248f9a870f
Alejandro Lucena <alelucena0836@gmail.com>
2023-07-11 18:58:31 -0400

    Create MongoDB Schema for storing using data with string data types

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  payment: { type: String },
+  college: { type: String },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
