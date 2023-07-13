664e0c44753d00cefeec5301d28271e562f98437
Alejandro Lucena <alelucena0836@gmail.com>
2023-07-13 15:28:21 -0400

    Added _id and uniqueness validation for username and email.

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  _id: Schema.Types.ObjectId,
+  username: { type: String, required: true, unique: true } ,
   name: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique:true },
   payment: { type: String },
   college: { type: String },
 });
