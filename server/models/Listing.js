9f07ed872d07f954d18b4af0e5f959c0515b0fb4
School <isaurosanchez0@gmail.com>
2023-07-13 20:33:53 -0500

    Added category and pickup variables

--- server/models/Listing.js
+++ server/models/Listing.js
@@ -1,13 +1,25 @@
 const { model, Schema } = require('mongoose');
 const Image = require('./Image.js')
 
+const categories = {
+    clothes: 0,
+    furniture: 1,
+    technology: 2,
+    jewelry: 3,
+    service: 4,
+    other: 5
+}
+
+
 
 const listingSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: String, required: true },
   active: { type: Boolean, required: true },
-  Pictures: [Image]
+  pictures: [Image],
+  pickup: Boolean,
+  category: categories
 });
 
 
