989982c60e48a27c1b51996da6d67fda1e14c0b0
Aaron Duong <duongaaron03@gmail.com>
2023-07-14 20:09:05 -0500

    added nav bar

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,35 +1,21 @@
-import owl from './owl.png';
-import './Landing.css';
+import owl from "./owl.png";
+import "./Landing.css";
+import {Navbar} from "./navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="Home.js">Home</a>
-          </li>
-
-          <li>
-            <a href="Favorites.js">Favorites</a>
-          </li>
-
-          <li>
-            <a href="Cart.js">Cart</a>
-          </li>
-        </ul>
-
-      </nav>
+      </Navbar>
 
       <header className="App-header">
-        <img src={owl} className="App-logo" alt="logo" style={{ width: '10vw', height: 'auto'}}/>
+        <img src={owl} className="App-logo" alt="logo" style={{ width: "10vw", height: "auto"}}/>
         <p>
-          Hello
+          Welcome to Owl Mart!
         </p>
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ client/src/components/pages/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+export const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box sx={{ display: "flex", justifyContent: "space-between", flexGrow: 1 }}>
+            <Typography variant="body1">Favorites</Typography>
+            <Typography variant="body1">Home</Typography>
+            <Typography variant="body1">Cart</Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
