c6c490adc279cb5b5eacd390ecb2bd13af1ad81e
Aaron Duong <duongaaron03@gmail.com>
2023-07-16 16:15:44 -0500

    slight navbar re-ordering

--- client/src/components/pages/navbar.js
+++ client/src/components/pages/navbar.js
@@ -8,6 +8,10 @@ export const Navbar = () => {
       <Toolbar>
         <Box sx={{ display: "flex", justifyContent: "space-between", flexGrow: 1 }}>
 
+        <Link to='/home'>
+              <Typography variant="body1">Home</Typography>
+            </Link>
+
           <Link to='/favorites'>
               <Typography variant="body1">Favorites</Typography>
             </Link>
@@ -17,9 +21,6 @@ export const Navbar = () => {
               <Typography variant="body1">Cart</Typography>
             </Link>
 
-            <Link to='/home'>
-              <Typography variant="body1">Home</Typography>
-            </Link>
 
         </Box>
       </Toolbar>
