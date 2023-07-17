417cbc80a05d56a4ece2057c9f0d4d2fc061661e
Aaron Duong <duongaaron03@gmail.com>
2023-07-17 16:24:39 -0500

    moved navbar to homepage

--- client/src/components/pages/navbar.js
+++ client/src/components/pages/navbar.js
@@ -1,27 +1,21 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Box sx={{ display: "flex", justifyContent: "space-between", flexGrow: 1 }}>
-
-        <Link to='/home'>
-              <Typography variant="body1">Home</Typography>
-            </Link>
-
-          <Link to='/favorites'>
-              <Typography variant="body1">Favorites</Typography>
-            </Link>
-
-
-            <Link to='/cart'>
-              <Typography variant="body1">Cart</Typography>
-            </Link>
-
-
+          <Link to="/home">
+            <Typography variant="body1">Home</Typography>
+          </Link>
+          <Link to="/favorites">
+            <Typography variant="body1">Favorites</Typography>
+          </Link>
+          <Link to="/cart">
+            <Typography variant="body1">Cart</Typography>
+          </Link>
         </Box>
       </Toolbar>
     </AppBar>
