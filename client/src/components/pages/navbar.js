97b26a496cf057a50b3cb16a932bf72c007bf3bc
Aaron Duong <duongaaron03@gmail.com>
2023-07-18 23:03:58 -0500

    favorites icon + progress check

--- client/src/components/pages/navbar.js
+++ client/src/components/pages/navbar.js
@@ -6,6 +6,8 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import MessageIcon from '@mui/icons-material/Message';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
+import favoritesIcon from './favorites-icon.png';
+
 
 export const Navbar = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -15,41 +17,38 @@ export const Navbar = () => {
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+    <AppBar position="static" sx={{ backgroundColor: 'white', boxShadow: 'none'}}>
       <Toolbar>
-        <Box sx = {{gap: "3rem", display: "flex", alignItems: "center",}}>
-          <Box sx={{ display: "flex", justifyContent: "space-between", flexGrow: 1 }}>
-            <Link to='/home' style={{ textDecoration: 'none' }}>
-              <Typography variant="h1" sx={{ fontSize: '2.4rem', fontFamily: 'Arial', fontWeight: 'bold', color: "#2E4561" }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100vw' }}>
+          <Box>
+            <Link to="/home" style={{ textDecoration: 'none' }}>
+              <Typography variant="h1" sx={{ fontSize: '2.4rem', fontFamily: 'Arial', fontWeight: 'bold', color: '#2E4561' }}>
                 OWLMART
               </Typography>
             </Link>
           </Box>
 
-          <Box sx={{ display: "flex", alignItems: "center",}}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <TextField
               id="search-bar"
               variant="outlined"
               size="small"
-            
               placeholder="Search for items or sellers"
               value={searchValue}
               onChange={handleSearchChange}
               sx={{
-                width: '30rem',
+                width: '25rem',
                 '& .MuiOutlinedInput-root': {
                   borderRadius: '20px',
                   backgroundColor: 'transparent',
                   '& fieldset': {
                     borderColor: '#2E4561',
-                    
                   },
                 },
                 '& input::-webkit-input-placeholder': {
                   color: '#9EB8D7',
                 },
-                color:"white",
-
+                color: 'white',
               }}
               InputProps={{
                 startAdornment: (
@@ -61,20 +60,21 @@ export const Navbar = () => {
                 ),
               }}
             />
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "20em", }}>
-              <IconButton >
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton >
-                <MessageIcon />
-              </IconButton>
-              <IconButton >
-                <AddIcon />
-              </IconButton>
-              <Button variant="contained" color="success" sx={{borderRadius: "20px"}}>
-                Log in
-              </Button>
+          </Box>
+
+          <Box sx={{display: 'flex', gap: '2rem'}}>
+            <IconButton>
+              <img src={favoritesIcon}  alt="favorites" style={{ width: 'auto', height: 'auto'}}/>
+            </IconButton>
+            <IconButton>
+              <MessageIcon />
+            </IconButton>
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+            <Button variant="contained" color="success" sx={{ whiteSpace: 'nowrap', borderRadius: '20px', textTransform: 'none', backgroundColor: "#2E4561", fontFamily: "Arial", fontSize: "1.1rem" , fontWeight: "bold"}}>
+              Log In
+            </Button>
           </Box>
         </Box>
       </Toolbar>
