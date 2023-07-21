f4a68bb1dc08972927b4b0420c2c5172bd6d3bff
Aaron Duong <duongaaron03@gmail.com>
2023-07-21 14:29:12 -0500

    filters section barebones

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -10,6 +10,11 @@ import favoritesIcon from "../../assets/favorites-icon.svg";
 const listingsData = [
   { id: 1, title: 'Item Name', seller: "aaron", description: "very good", price: '$100.00', image: Owl, pickup: 'on-campus', category: 'insert tags'},
   { id: 2, title: 'Item Name 2', seller: 'John Doe', description: "worth an investment", price: '$50.50', image: test, pickup: 'off-campus', category: 'insert tags' },
+  { id: 2, title: 'Item Name 2', seller: 'John Doe', description: "worth an investment", price: '$50.50', image: test, pickup: 'off-campus', category: 'insert tags' },
+  { id: 2, title: 'Item Name 2', seller: 'John Doe', description: "worth an investment", price: '$50.50', image: test, pickup: 'off-campus', category: 'insert tags' },
+  { id: 2, title: 'Item Name 2', seller: 'John Doe', description: "worth an investment", price: '$50.50', image: test, pickup: 'off-campus', category: 'insert tags' },
+  { id: 2, title: 'Item Name 2', seller: 'John Doe', description: "worth an investment", price: '$50.50', image: test, pickup: 'off-campus', category: 'insert tags' },
+
 ];
 
 const CardComponent = ({ image, title, price, seller }) => {
@@ -34,17 +39,17 @@ const CardComponent = ({ image, title, price, seller }) => {
             {title}
           </Typography>
   
-          <Typography sx={{ fontFamily: "Lato-bold", fontSize: "1.1rem", marginBottom: '0.5rem' }}>
+          <Typography sx={{ fontFamily: "Lato-bold", fontSize: "1.3rem", marginBottom: '0.5rem' }}>
             Price: {price}
           </Typography>
   
-          <Typography sx={{ fontFamily: "Libre-regular", fontSize: "1.5rem" }}>
-            Seller: {seller}
+          <Typography sx={{ fontFamily: "Libre-regular", fontSize: "1rem" }}>
+            {seller}
           </Typography>
         </CardContent>
   
         <Box display="flex" justifyContent="space-between" alignItems="center" marginLeft="1rem" marginBottom="1rem">
-          <Button variant="contained" sx={{ color: '#141517', backgroundColor: '#B6CEEA', fontFamily: "Lato-bold", fontSize: "1.1rem", textTransform: 'none' }}>
+          <Button variant="contained" sx={{ color: '#141517', backgroundColor: '#B6CEEA', fontFamily: "Lato-regular", fontSize: "1.1rem", textTransform: 'none' }}>
             Contact Seller
           </Button>
   
@@ -61,28 +66,54 @@ const CardComponent = ({ image, title, price, seller }) => {
   );
 };
 
-const ProductGrid = () => {
+const Products = () => {
   return (
-    <Grid container spacing={2}>
-      {listingsData.map((listing) => (
-        <Grid item key={listing.id} xs={8} sm={6} md={4} lg={3}>
-          <CardComponent
-            title={listing.title}
-            price={listing.price}
-            image={listing.image}
-            seller={listing.seller}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Box display="flex" width="50vw" marginRight="5vw" marginTop = "10vh">
+      <Grid container spacing={3}>
+        {listingsData.map((listing) => (
+          <Grid item key={listing.id} xs={12} sm={6} md={4} lg={4}>
+            <CardComponent
+              title={listing.title}
+              price={listing.price}
+              image={listing.image}
+              seller={listing.seller}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
+
+
+const Filters = () => {
+  return (
+  <Box sx={{ backgroundColor: "green", height: "100vh", width: "20vw", marginLeft: '5vw' }}>
+    <Typography sx={{ fontFamily: "Lato-Bold", fontSize: "3rem" }}>
+      Items
+    </Typography>
+
+    <Typography sx={{ fontFamily: "Lato-regular", fontSize: "1rem", marginBottom: '0.5rem', marginTop: '1rem' }}>
+      Filters
+    </Typography>
+  </Box>
+
+    
+
+
+  )
+}
+
+
 
 export const HomePage = () => {
   return (
     <>
       <h1>This is the home page</h1>
-      <ProductGrid />
+      <Box display = "flex" justifyContent="space-between">
+       <Filters />
+        <Products />
+      </Box>
     </>
   );
 };
