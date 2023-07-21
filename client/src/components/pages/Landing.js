7a062cd3acffc4d0fbd7bef423d55bfced15f975
Aaron Duong <duongaaron03@gmail.com>
2023-07-20 22:16:55 -0500

    fixed merge conflicts

---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,35 @@
+import owl from './owl.png';
+import './Landing.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav>
+        <ul>
+          <li>
+            <a href="Home.js">Home</a>
+          </li>
+
+          <li>
+            <a href="Favorites.js">Favorites</a>
+          </li>
+
+          <li>
+            <a href="Cart.js">Cart</a>
+          </li>
+        </ul>
+
+      </nav>
+
+      <header className="App-header">
+        <img src={owl} className="App-logo" alt="logo" style={{ width: '10vw', height: 'auto'}}/>
+        <p>
+          Hello
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
