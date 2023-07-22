8d7025ff3c73a8b03268931d81b20f4194977dff
Alejandro Lucena <alelucena0836@gmail.com>
2023-07-22 14:56:36 -0400

    Added deleteUser mutation

--- server/graphql/resolvers/user.js
+++ server/graphql/resolvers/user.js
@@ -27,11 +27,21 @@ Mutation: {
         if(!(email.LowerCase.includes('rice.edu'))) {
             throw new Error('Non-Rice email: invalid registration credentials')
         }
-       
-
+        
         const user = await newUser.save();
             
         return user;
+    },
+    deleteUser: async (_, { userId }, context) => {
+      // TODO: AUTHENTICATE BEFORE DELETING DOG
+
+      try {
+        const deletedUser = await User.findOneAndDelete({ _id: userId });
+        if (deletedUser) return 'User deleted Sucessfully';
+        else throw new Error('User does not exist');
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
