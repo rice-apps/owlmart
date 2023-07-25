cd94c80f614e6b2a5577f991b9f7309c75a8e801
Alejandro Lucena <alelucena0836@gmail.com>
2023-07-25 15:03:45 -0400

    Removed unnecessary comments and added extra email validation

--- server/graphql/resolvers/user.js
+++ server/graphql/resolvers/user.js
@@ -1,17 +1,24 @@
 const { AuthenticationError } = require('apollo-server');
 const User = require('../../models/User');
 const bcrypt = require('bcrypt');
-//const { MONGODB } = require('../../../config');
-//var MongoClient = require("mongodb").MongoClient;
-//var url = MONGODB
 
+//Email validation
+function ValidateEmail(inputText) 
+{
+	var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	if(inputText.value.match(mailformat)) 
+  {
+		alert("This is not a valid email address");
+		return false;
+	}
+}
 module.exports = {
 
 Mutation: {
-    createUser: async (_, { netID }, context) => {
+    createUser: async (_, { netID, firstName, middleInitital, lastName, password, email, payment, college }, context) => {
         
         const newUser = new User ({
-            netID, first_name, middle_initital, last_name,
+            netID, firstName, middleInitital, lastName,
              password, email, payment, college,
         });
 
@@ -23,15 +30,28 @@ Mutation: {
         if (netID.trim() === '') {
             throw new Error('NetID must not be empty');
           }
-        // Non-valid email error
-        if(!(email.LowerCase.includes('rice.edu'))) {
-            throw new Error('Non-Rice email: invalid registration credentials')
-        }
-       
 
+        // Non-valid email error
+        if(( ValidateEmail(email) == false && email.includes('@rice.edu')) == false) {
+            throw new Error('Non-valid email: invalid registration credentials')
+        }
+      
+        
         const user = await newUser.save();
             
         return user;
     },
+    deleteUser: async (_, { userId }, context) => {
+      // TODO: AUTHENTICATE BEFORE DELETING DOG
+
+      try {
+        const deletedUser = await User.findOneAndDelete({ _id: userId });
+        if (deletedUser) return 'User deleted Sucessfully';
+        else throw new Error('User does not exist');
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
+
