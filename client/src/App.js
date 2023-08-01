fa4f3abe612a5c7eed1340fd4a885907bf5a1149
Aaron Duong <duongaaron03@gmail.com>
2023-08-01 17:39:25 -0500

    removed test.png

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/pages/HomePage.js';
-import { Cart } from './components/pages/Cart.js';
 import { Favorites } from './components/pages/Favorites.js';
 import { LandingPage } from './components/pages/LandingPage';
 import ProductDetail from './components/pages/ProductDetail.js';
