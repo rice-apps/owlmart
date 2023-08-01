1363374d67660d5e3ec0f415311e23213e63d242
joanil17 <your@email.com>
2023-08-01 13:38:29 -0500

    duplicate in al products

--- client/src/components/pages/AllProducts.js
+++ client/src/components/pages/AllProducts.js
@@ -1,20 +1,6 @@
 import Owl from './owl.png';
 import test from './test.png';
 const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-const listingsData = [
-    { id: 1, title: 'Item Name', seller: 'Anya', description: desc, price: '$20.00', image: "https://img.buzzfeed.com/buzzfeed-static/static/2017-10/31/13/asset/buzzfeed-prod-fastlane-01/sub-buzz-20342-1509470849-7.jpg?crop=600%3A900%3B0%2C0&downsize=300:*&output-format=auto&output-quality=auto", pickup: 'On Campus', category: 'insert tags'},
-    { id: 2, title: 'Item Name', seller: 'Anya', description: desc, price: '$35.50', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqORus3Fnu7SPQIVsONI8i5_C02zJmxOfldg&usqp=CAU", pickup: 'Off Campus', category: 'insert tags' },
-    { id: 3, title: 'Item Name', seller: 'Anya', description: desc, price: '$60.50', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzVUSex0mTKssYWsQr4fw9pwA0-jQssO3qkg&usqp=CAU", pickup: 'On Campus', category: 'insert tags' },
-    { id: 4, title: 'Item Name', seller: 'Anya', description: desc, price: '$75.00', image: "https://img.buzzfeed.com/buzzfeed-static/static/2019-03/18/13/asset/buzzfeed-prod-web-04/sub-buzz-18824-1552930682-1.jpg?downsize=600:*&output-format=auto&output-quality=auto", pickup: 'Off Campus', category: 'insert tags' },
-    { id: 5, title: 'Item Name', seller: 'Anya', description: desc, price: '$45.30', image: "https://img.buzzfeed.com/buzzfeed-static/static/2019-03/18/13/asset/buzzfeed-prod-web-04/sub-buzz-18897-1552930656-1.jpg?downsize=600:*&output-format=auto&output-quality=auto", pickup: 'On Campus', category: 'insert tags' },
-    { id: 6, title: 'Item Name', seller: 'Daniel', description: desc, price: '$18.00', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwWLY_VGp0XpmXZwTFqVFgtJR3YnQE1YeBZw&usqp=CAU", pickup: 'Off Campus', category: 'insert tags' },
-    { id: 7, title: 'Item Name', seller: 'Daniel', description: desc, price: '$55.00', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSSEQBCKJBz_sOI5QEpn6Mvf8CXH1zd6Olxg&usqp=CAU", pickup: 'On Campus', category: 'insert tags' },
-    { id: 8, title: 'Item Name', seller: 'Daniel', description: desc, price: '$80.00', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN-Dvr18OTFKQBQoPcCByCZA9MIYXFv8zNxg&usqp=CAU", pickup: 'Off Campus', category: 'insert tags' },
-    { id: 9, title: 'Item Name', seller: 'Daniel', description: desc, price: '$15.00', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Au7GbbNI31mViu481D0xtWYWJPafkzEHOA&usqp=CAU", pickup: 'On Campus', category: 'insert tags' },
-    { id: 10, title: 'Item Name', seller: 'Daniel', description: desc, price: '$90.00', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0NlyeXk5srB91GTuUPYHg22jCAwEp8vuQ0Q&usqp=CAU", pickup: 'Off Campus', category: 'insert tags' },
-  
-
 const listingsData = [
     { id: 1, title: 'Wonderful Item', seller: 'Anya', description: 'Top-notch quality', price: '$20.00', image: test, pickup: 'On Campus', category: 'insert tags'},
     { id: 2, title: 'Fantastic Good', seller: 'Anya', description: 'Worth every penny', price: '$35.50', image: Owl, pickup: 'Off Campus', category: 'insert tags' },
