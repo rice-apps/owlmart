708641a72e48105c48652e8c09c407e3f2434e57
Aaron Duong <duongaaron03@gmail.com>
2023-08-01 18:42:24 -0500

    added listing mutation updates

--- server/graphql/resolvers/listing.js
+++ server/graphql/resolvers/listing.js
@@ -15,6 +15,20 @@ function isValid_Currency(str) {
     }
 }
 
+// Checks if str is in a valid US currency format
+function isValid_Currency(str) {
+    // regex to check valid US currency
+    let regex = new RegExp(/^\$(\d{1, 3}(\, \d{3})*|(\d+))(\.\d{2})?$/);
+
+    // returns true if the str matched the ReGex
+    if (regex.test(str) == true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 module.exports = {
   Query: {
     async getListings() {
@@ -44,8 +58,6 @@ module.exports = {
   Mutation: {
     createListing: async (_, { title, description, price, active, pictures, pickup, category }, context) => {
       
-      // Check for empty strings
-      // Check for empty strings
       // Check for empty strings
       if (title.trim() === '') {
         throw new Error('Title must not b empty');
@@ -102,42 +114,6 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
-    updateListing: async (_, { listingId, title, description, price, active, pictures, pickup, category }, context) => {
-      // Create an empty updates object
-      let updates = {};
-    
-      // Only add fields to the updates object if they are not undefined (i.e., provided in the mutation arguments)
-      if (title !== undefined) updates.title = title;
-      if (description !== undefined) updates.description = description;
-      if (price !== undefined) {
-        if (!isValid_Currency(price)) {
-          throw new Error("Price is not in a valid format, don't forget the $!");
-        }
-        updates.price = price;
-      }
-      if (active !== undefined) updates.active = active;
-      if (pictures !== undefined) updates.pictures = pictures;
-      if (pickup !== undefined) updates.pickup = pickup;
-      if (category !== undefined) updates.category = category;
-    
-      try {
-        // Use the updates object to update the MongoDB document.
-        // Fields not included in the updates object will not be affected.
-        const updatedListing = await Listing.findByIdAndUpdate(
-          listingId,
-          updates,
-          { new: true }
-        );
-    
-        if (!updatedListing) {
-          throw new Error('Listing not found');
-        }
-    
-        return updatedListing;
-      } catch (err) {
-        throw new Error(err);
-      }
     }
   }
-}
 }
