fea04e814a1f156b15be9d6ad72984ba4267e5d2
Aaron Duong <duongaaron03@gmail.com>
2023-08-02 23:21:42 -0500

    merged landingstyles into landingpage using MUI instead of styled

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -1,19 +1,63 @@
 import { Link } from "react-router-dom";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Box } from "@mui/material";
 import Navbar from "../Navbar";
-import {
-  CenteredBox,
-  DescriptionBox,
-  TitleBox,
-  StyledButton,
-} from "./LandingStyles";
+
+const iphoneMaxWidth = "768px";
+
 export const LandingPage = () => {
   return (
     <div className="App">
       <Navbar />
-      <CenteredBox>
-        <DescriptionBox>
-          <TitleBox>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+          width: "100vw",
+          [`@media (max-width: ${iphoneMaxWidth})`]: {
+            height: "60vh",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            backgroundColor: "#9eb8d7",
+            color: "black",
+            height: "47vh",
+            width: "88vw",
+            borderRadius: "30px",
+            fontFamily: "Libre Baskerville",
+            textAlign: "center",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "normal",
+            [`@media (max-width: ${iphoneMaxWidth})`]: {
+              height: "35vh",
+            },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "10vh",
+              width: "50vw",
+              fontFamily: "Lato-bold",
+              color: "black",
+              fontSize: "2rem",
+              [`@media (max-width: ${iphoneMaxWidth})`]: {
+                fontSize: "1.5rem",
+              },
+            }}
+          >
             <Typography
               variant="h1"
               sx={{
@@ -27,20 +71,20 @@ export const LandingPage = () => {
             >
               OWLMART
             </Typography>
-          </TitleBox>
+          </Box>
           Owl Mart is a web application designed to facilitate buying and
           selling among Rice University students. It resembles a shopping app
           and allows students to sell various products such as furniture,
           clothing, accessories, etc.
           <Button
             variant="contained"
-            style={{
+            component={Link}
+            to="/home"
+            sx={{
               backgroundColor: "white",
               borderRadius: "15px",
               marginTop: "10vh",
             }}
-            component={Link}
-            to="/home"
           >
             <Typography
               variant="h1"
@@ -57,8 +101,23 @@ export const LandingPage = () => {
               Shop Now
             </Typography>
           </Button>
-        </DescriptionBox>
-        <TitleBox style={{ marginTop: "10vh" }}>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "10vh",
+            width: "50vw",
+            fontFamily: "Lato-bold",
+            color: "black",
+            fontSize: "2rem",
+            marginTop: "10vh",
+            [`@media (max-width: ${iphoneMaxWidth})`]: {
+              fontSize: "1.5rem",
+            },
+          }}
+        >
           <Typography
             variant="h1"
             sx={{
@@ -72,10 +131,10 @@ export const LandingPage = () => {
           >
             Want to start selling?
           </Typography>
-        </TitleBox>
+        </Box>
         <Button
           variant="contained"
-          style={{
+          sx={{
             backgroundColor: "#B6CEEA",
             borderRadius: "15px",
             color: "black",
@@ -90,7 +149,7 @@ export const LandingPage = () => {
         >
           Sign Up
         </Button>
-      </CenteredBox>
+      </Box>
     </div>
   );
 };
